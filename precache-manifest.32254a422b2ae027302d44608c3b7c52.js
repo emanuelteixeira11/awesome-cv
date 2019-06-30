9de0f41c6830113963d6688e0a6e0d9157ba32ff
Emanuel Teixeira <eteixeira11@hotmail.com>
2019-06-30 19:51:01 +0100

    Auto-generated commit

---
+++ precache-manifest.32254a422b2ae027302d44608c3b7c52.js
@@ -0,0 +1,302 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025467f9da66693700b86905ea2d0ea1",
+    "url": "./i18n/en.json"
+  },
+  {
+    "revision": "59a9afdbd3d057a768f2899a805574dd",
+    "url": "./i18n/pt-pt.json"
+  },
+  {
+    "url": "app/0.10605c46c0eaf2269718.chunk.js"
+  },
+  {
+    "url": "app/1.10605c46c0eaf2269718.chunk.js"
+  },
+  {
+    "url": "app/10.10605c46c0eaf2269718.chunk.js"
+  },
+  {
+    "url": "app/11.10605c46c0eaf2269718.chunk.js"
+  },
+  {
+    "url": "app/12.10605c46c0eaf2269718.chunk.js"
+  },
+  {
+    "url": "app/13.10605c46c0eaf2269718.chunk.js"
+  },
+  {
+    "url": "app/14.10605c46c0eaf2269718.chunk.js"
+  },
+  {
+    "url": "app/15.10605c46c0eaf2269718.chunk.js"
+  },
+  {
+    "url": "app/16.10605c46c0eaf2269718.chunk.js"
+  },
+  {
+    "url": "app/5.10605c46c0eaf2269718.chunk.js"
+  },
+  {
+    "url": "app/6.10605c46c0eaf2269718.chunk.js"
+  },
+  {
+    "url": "app/7.10605c46c0eaf2269718.chunk.js"
+  },
+  {
+    "url": "app/8.10605c46c0eaf2269718.chunk.js"
+  },
+  {
+    "url": "app/9.10605c46c0eaf2269718.chunk.js"
+  },
+  {
+    "url": "app/global.10605c46c0eaf2269718.bundle.js"
+  },
+  {
+    "url": "app/main.10605c46c0eaf2269718.bundle.js"
+  },
+  {
+    "url": "app/polyfills.10605c46c0eaf2269718.bundle.js"
+  },
+  {
+    "revision": "3fc3e8ec3f4615853005eedcffba2f73",
+    "url": "content/.DS_Store"
+  },
+  {
+    "url": "content/4335d5f923e59343903e1b055ef4570c.svg"
+  },
+  {
+    "url": "content/a91a768a67aba7d94a38279e980f9284.png"
+  },
+  {
+    "revision": "8eb1ca59e24ef9a12d58bd8e76f4b105",
+    "url": "content/css/loading.css"
+  },
+  {
+    "url": "content/d46cc6d507235c9837c24531ead103fa.png"
+  },
+  {
+    "revision": "d05aaaddfb20752612f1",
+    "url": "content/global.be4171ccda4560361559.css"
+  },
+  {
+    "revision": "fd246ffaf972eb02eea9794464cf3014",
+    "url": "content/images/awesome_cv_logo.png"
+  },
+  {
+    "revision": "a91a768a67aba7d94a38279e980f9284",
+    "url": "content/images/icon_cv_landing.png"
+  },
+  {
+    "revision": "fc8bbf677d4bc9aa695128e317826c11",
+    "url": "content/images/jhipster_family_member_0.svg"
+  },
+  {
+    "revision": "60153167157b81ab27501ae8b31894ad",
+    "url": "content/images/jhipster_family_member_0_head-192.png"
+  },
+  {
+    "revision": "d03f8bd995f0a00138c980166e4e1f4f",
+    "url": "content/images/jhipster_family_member_0_head-256.png"
+  },
+  {
+    "revision": "5e42e89481bd0d871127bcf1b71f534b",
+    "url": "content/images/jhipster_family_member_0_head-384.png"
+  },
+  {
+    "revision": "9e95e063adf68e56de24f9b07f18724d",
+    "url": "content/images/jhipster_family_member_0_head-512.png"
+  },
+  {
+    "revision": "d47e50fffba79727e1f2bf92c90bd261",
+    "url": "content/images/jhipster_family_member_1.svg"
+  },
+  {
+    "revision": "d7615bfd618c6a38467ea5dcae0c843c",
+    "url": "content/images/jhipster_family_member_1_head-192.png"
+  },
+  {
+    "revision": "873cd501055f181be091f0ce6fae66b7",
+    "url": "content/images/jhipster_family_member_1_head-256.png"
+  },
+  {
+    "revision": "933594b228e47c82e8b9984f1aaac02f",
+    "url": "content/images/jhipster_family_member_1_head-384.png"
+  },
+  {
+    "revision": "8dcb2ce4d40cb6bb12bfd0e1d5a25d29",
+    "url": "content/images/jhipster_family_member_1_head-512.png"
+  },
+  {
+    "revision": "4335d5f923e59343903e1b055ef4570c",
+    "url": "content/images/jhipster_family_member_2.svg"
+  },
+  {
+    "revision": "e905c8dc4e6b7f684f8f82bcbd3f9ae8",
+    "url": "content/images/jhipster_family_member_2_head-192.png"
+  },
+  {
+    "revision": "58eee5b720660310d4b8e2eaee52b8e6",
+    "url": "content/images/jhipster_family_member_2_head-256.png"
+  },
+  {
+    "revision": "1bcb1974aa152cb90bb6dc4440d9b6eb",
+    "url": "content/images/jhipster_family_member_2_head-384.png"
+  },
+  {
+    "revision": "46cf6f8200c6961922a90bccf06d31ae",
+    "url": "content/images/jhipster_family_member_2_head-512.png"
+  },
+  {
+    "revision": "1d1a37383d4e4fddbf7e5bcf6413dc09",
+    "url": "content/images/jhipster_family_member_3.svg"
+  },
+  {
+    "revision": "e91ab0d2cc758e357afde72a8d415f5f",
+    "url": "content/images/jhipster_family_member_3_head-192.png"
+  },
+  {
+    "revision": "6568b844c5f5618b9e88e2a4651fde6e",
+    "url": "content/images/jhipster_family_member_3_head-256.png"
+  },
+  {
+    "revision": "f2c800ed8dab0ef8b46cb2c8feee1f83",
+    "url": "content/images/jhipster_family_member_3_head-384.png"
+  },
+  {
+    "revision": "216bc67175949d4ec113e3382d2eb3fd",
+    "url": "content/images/jhipster_family_member_3_head-512.png"
+  },
+  {
+    "revision": "d46cc6d507235c9837c24531ead103fa",
+    "url": "content/images/logo-jhipster.png"
+  },
+  {
+    "revision": "479e017d7af7d9cd4793",
+    "url": "content/main.77e5f3e37ecd794bbd5d.css"
+  },
+  {
+    "revision": "43c4c5e01296a0ad299964e48e8de331",
+    "url": "content/scss/_bootstrap-variables.scss"
+  },
+  {
+    "revision": "5680e9e1dc319647622e47f7170e360e",
+    "url": "content/scss/global.scss"
+  },
+  {
+    "revision": "e1fd775435d8be096c6b01d16602ae6e",
+    "url": "content/scss/vendor.scss"
+  },
+  {
+    "revision": "ff52e0622ee1be5cdef1f150b3c7467c",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "f678fad9459914b3501a782c9e81c03d",
+    "url": "index.html"
+  },
+  {
+    "revision": "7718cca994e6320417dc34845d3b7834",
+    "url": "manifest.webapp"
+  },
+  {
+    "revision": "d4a5d598e8510d03a187a3a2b8bda2e0",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "8cc912f865f2a9260a466a763cb4f325",
+    "url": "swagger-ui/dist/css/print.css"
+  },
+  {
+    "revision": "9e41060781703b7b6492b418708f2ef3",
+    "url": "swagger-ui/dist/css/reset.css"
+  },
+  {
+    "revision": "421e5f1e932e9492960bf5ee003a2bf5",
+    "url": "swagger-ui/dist/css/screen.css"
+  },
+  {
+    "revision": "58f2be3ec002df70ee20e331f9f820b0",
+    "url": "swagger-ui/dist/css/style.css"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "swagger-ui/dist/css/typography.css"
+  },
+  {
+    "revision": "bfefe70e3951f1883a84e7bc4033fe97",
+    "url": "swagger-ui/dist/images/throbber.gif"
+  },
+  {
+    "revision": "a6bad0b6ac331975e0b24d9b66de956c",
+    "url": "swagger-ui/dist/lib/backbone-min.js"
+  },
+  {
+    "revision": "3b04ed35413837bb94e0c65593474e61",
+    "url": "swagger-ui/dist/lib/es5-shim.js"
+  },
+  {
+    "revision": "7cbf2531fdd0f6be992f372261d99f42",
+    "url": "swagger-ui/dist/lib/handlebars-4.0.5.js"
+  },
+  {
+    "revision": "b371d897bcd15136f45281d08e86d8b4",
+    "url": "swagger-ui/dist/lib/highlight.9.1.0.pack.js"
+  },
+  {
+    "revision": "14bcf82ebb6e20b315d226570ec747d0",
+    "url": "swagger-ui/dist/lib/highlight.9.1.0.pack_extended.js"
+  },
+  {
+    "revision": "f3a3912a4f6c5f301581c352d992a8ec",
+    "url": "swagger-ui/dist/lib/jquery-1.8.0.min.js"
+  },
+  {
+    "revision": "f8da0230b004766d380a2552b6c6bbf5",
+    "url": "swagger-ui/dist/lib/jquery.ba-bbq.min.js"
+  },
+  {
+    "revision": "44b316a633a6b7e2b58cf2020ed4fd54",
+    "url": "swagger-ui/dist/lib/jquery.slideto.min.js"
+  },
+  {
+    "revision": "47792a9414257158e5182cfaeaa1cd1d",
+    "url": "swagger-ui/dist/lib/jquery.wiggle.min.js"
+  },
+  {
+    "revision": "55b3b1cf977b730896918548ff4eb209",
+    "url": "swagger-ui/dist/lib/js-yaml.min.js"
+  },
+  {
+    "revision": "a322d44f9f3161a56b0c90deaaa43db4",
+    "url": "swagger-ui/dist/lib/jsoneditor.min.js"
+  },
+  {
+    "revision": "10a5aa0d437e9b25f17c7863fb0d91ce",
+    "url": "swagger-ui/dist/lib/lodash.min.js"
+  },
+  {
+    "revision": "36a9155a35fb31e1ce9cc40ae758396f",
+    "url": "swagger-ui/dist/lib/marked.js"
+  },
+  {
+    "revision": "32285bd59090f5179431e8c5a0a8373a",
+    "url": "swagger-ui/dist/lib/object-assign-pollyfill.js"
+  },
+  {
+    "revision": "853410b8a71feb711b7015b0747a930e",
+    "url": "swagger-ui/dist/lib/sanitize-html.min.js"
+  },
+  {
+    "revision": "0b77cfcaea934215c9480427cf0ae8f8",
+    "url": "swagger-ui/dist/lib/swagger-oauth.js"
+  },
+  {
+    "revision": "9b5e95be97f2e150767492edbf3d8e62",
+    "url": "swagger-ui/dist/swagger-ui.min.js"
+  },
+  {
+    "revision": "d4da0bd6244a377f9433155a2129421b",
+    "url": "swagger-ui/index.html"
+  }
+]);
